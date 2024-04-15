ef68e54f3bac709c12f1acacbc6117ca614f0b56
Jonathan Krall <jonathan.krall@lexicon.se>
2024-04-15 11:53:48 +0200

    Scaffolded controller and added Database

--- LexiconIMDB/Program.cs
+++ LexiconIMDB/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using LexiconIMDB.Data;
 namespace LexiconIMDB
 {
     public class Program
@@ -5,16 +8,20 @@ namespace LexiconIMDB
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<LexiconIMDBContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("LexiconIMDBContext") ?? throw new InvalidOperationException("Connection string 'LexiconIMDBContext' not found.")));
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Movie/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -28,7 +35,7 @@ namespace LexiconIMDB
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Movies}/{action=Index}/{id?}");
 
             app.Run();
         }
